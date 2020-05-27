152c1a7b8f67c9c9177e3467df45471b099808f8
barry <410547@gm.tnfsh.tn.edu.tw>
2020-05-27 17:16:19 +0800

    26

---
+++ check/XBee_host.py
@@ -0,0 +1,109 @@
+import serial
+
+import time
+
+
+# XBee setting
+
+serdev = '/dev/ttyUSB0'
+
+s = serial.Serial(serdev, 9600)
+
+
+s.write("+++".encode())
+
+char = s.read(2)
+
+print("Enter AT mode.")
+
+print(char.decode())
+
+
+s.write("ATMY 0x118\r\n".encode())
+
+char = s.read(3)
+
+print("Set MY 0x118.")
+
+print(char.decode())
+
+
+s.write("ATDL 0x218\r\n".encode())
+
+char = s.read(3)
+
+print("Set DL 0x218.")
+
+print(char.decode())
+
+
+s.write("ATID 0x1\r\n".encode())
+
+char = s.read(3)
+
+print("Set PAN ID 0x1.")
+
+print(char.decode())
+
+
+s.write("ATWR\r\n".encode())
+
+char = s.read(3)
+
+print("Write config.")
+
+print(char.decode())
+
+
+s.write("ATMY\r\n".encode())
+
+char = s.read(4)
+
+print("MY :")
+
+print(char.decode())
+
+
+s.write("ATDL\r\n".encode())
+
+char = s.read(4)
+
+print("DL : ")
+
+print(char.decode())
+
+
+s.write("ATCN\r\n".encode())
+
+char = s.read(3)
+
+print("Exit AT mode.")
+
+print(char.decode())
+
+
+print("start sending RPC")
+
+time.sleep(1)
+s.write("/myled1/write 1\r".encode())
+
+
+
+
+s.write("/myled2/write 1\r".encode())
+
+
+s.write("/myled3/write 1\r".encode())
+
+
+while True:
+
+    # send RPC to remote
+
+    s.write("/getAcc/run\r".encode())
+    time.sleep(1)
+
+  
+
+
+s.close()
